ab7dfa0758bb0e914e7619dff6a3635a449de997
itzadetunji <adetunjiadeyinka29@gmail.com>
2026-08-17 13:17:42 +0100

    Update dependencies, modify Navbar component, and enhance UI styles. Added new font sources, updated button and input components to use @base-ui/react, and improved styling for various UI elements including sliders and switches.

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,8 +1,10 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useLocation } from "@tanstack/react-router";
 import React from "react";
 import { Button } from "../ui/button";
+import { cn } from "#/lib/utils";
 
 export const Navbar: React.FC = () => {
+	const { pathname } = useLocation();
 	return (
 		<nav className="flex items-center justify-between max-w-360 mx-auto w-full px-16 py-2">
 			<div className="flex items-center gap-2">
@@ -10,20 +12,43 @@ export const Navbar: React.FC = () => {
 					<div className="w-25 h-18 bg-black/10"></div>
 				</Link>
 			</div>
-			<ul className="flex items-center gap-2">
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
+			<ul className="flex items-center gap-10">
+				{NAV_LINKS.map((link) => (
+					<li key={link.to}>
+						<Link
+							to={link.to}
+							className={cn(
+								"hover:text-primary duration-300 ease-in-out hover:font-semibold text-muted-foreground",
+								link.to === pathname && "text-primary font-medium",
+							)}
+						>
+							{link.label}
+						</Link>
+					</li>
+				))}
 			</ul>
 			<Button
-				variant="outline"
-				asChild
+				nativeButton={false}
+				render={<Link to="/contact" />}
+				className="font-normal py-6 px-6"
 			>
-				<Link to="/contact">Contact</Link>
+				Contact Us
 			</Button>
 		</nav>
 	);
 };
+
+const NAV_LINKS = [
+	{
+		label: "Home",
+		to: "/",
+	},
+	{
+		label: "About Us",
+		to: "/about",
+	},
+	{
+		label: "Contact",
+		to: "/contact-us",
+	},
+];
