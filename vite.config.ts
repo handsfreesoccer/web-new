8ceec024f9578c8c18c589aba4410e7f8e27f4a1
itzadetunji <adetunjiadeyinka29@gmail.com>
2026-08-19 18:47:37 +0100

    Add nitro package and integrate into Vite configuration; update bun.lock and package.json for dependency management.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  server: {
-    host: true,
-    port: Number(process.env.PORT) || 5173,
-    strictPort: true,
-  },
-  ssr: {
-    external: ['bun'],
-  },
-  optimizeDeps: {
-    exclude: ['bun'],
-  },
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		devtools(),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		nitro(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+	server: {
+		host: true,
+		port: Number(process.env.PORT) || 5173,
+		strictPort: true,
+	},
+	ssr: {
+		external: ["bun"],
+	},
+	optimizeDeps: {
+		exclude: ["bun"],
+	},
+});
 
-export default config
+export default config;
